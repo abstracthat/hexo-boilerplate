8858bedaf00f045754ca9ae1f793758e4237a65c
Vyacheslav Levit <levit.slava@gmail.com>
2014-05-22 23:36:58 +0300

    Filter the tag list for only tags in the category

--- themes/boilerplate/scripts/helpers.js
+++ themes/boilerplate/scripts/helpers.js
@@ -1,5 +1,7 @@
 /*global hexo */
 
+var _ = require('lodash');
+
 function tag_to_class(tag_name) {
     return 'tag-' + tag_name.replace(/\s/g, '-');
 }
@@ -12,4 +14,14 @@ hexo.extend.helper.register('tags_to_class', function(tags){
         cls += tag_to_class(tag.name) + ' ';
     });
     return cls;
+});
+
+hexo.extend.helper.register('category_tags', function(category) {
+    var tags = [];
+    var cat = Category.first({name: category}).populate('posts');
+    cat.posts.each(function(post) {
+        tags = _.union(tags, post.tags);
+    });
+    tags = Tag.find({_id: {$in: tags}}).toArray();
+    return tags;
 });
