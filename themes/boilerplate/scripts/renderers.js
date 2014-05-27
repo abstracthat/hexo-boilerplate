fe153f506d5b0bc4a305159b8d85ab65f6a0f84c
Vyacheslav Levit <levit.slava@gmail.com>
2014-05-28 01:43:50 +0300

    Install jeet, axis, rupture, autoprefixer

---
+++ themes/boilerplate/scripts/renderers.js
@@ -0,0 +1,24 @@
+var stylus = require('stylus'),
+  nib = require('nib'),
+  axis = require('axis-css'),
+  jeet = require('jeet'),
+  rupture = require('rupture'),
+  autoprefixer = require('autoprefixer-stylus');
+
+var stylus_renderer = function(data, options, callback){
+  var stylus_config = hexo.config.stylus || {};
+
+  stylus(data.text)
+    .use(nib())
+    .use(axis())
+    .use(jeet())
+    .use(rupture())
+    .use(autoprefixer())
+    .set('filename', data.path)
+    .set('compress', stylus_config.compress)
+    .set('include css', true)
+    .render(callback);
+};
+
+hexo.extend.renderer.register('styl', 'css', stylus_renderer);
+hexo.extend.renderer.register('stylus', 'css', stylus_renderer);
